d71bce082001019edb810aaede3fd7d1fc11dbed
jlks96 <joshlee_1996@hotmail.com>
2020-06-08 05:23:46 +0800

    Updates

---
+++ precache-manifest.41755ef9ceb6bef11b7fdc218c7e54bb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67ff6ec0589e2cc174b8d8381710aa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f9c00c4a29735b28090",
+    "url": "/static/css/main.c3ce5744.chunk.css"
+  },
+  {
+    "revision": "55026c5fec1f80d6f01e",
+    "url": "/static/js/2.d42647ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d42647ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9c00c4a29735b28090",
+    "url": "/static/js/main.5a943c49.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "4820e39433f321d07fa484ec611187af",
+    "url": "/static/media/TICC.4820e394.jpg"
+  },
+  {
+    "revision": "d182cc1d59847f0fd36303540e6bc00a",
+    "url": "/static/media/addsurance.d182cc1d.png"
+  },
+  {
+    "revision": "81ac5345eb2683c93250f781036e15ce",
+    "url": "/static/media/graphics.81ac5345.png"
+  },
+  {
+    "revision": "a2c4a72bf2b8c4aaf12704ef1f6518fc",
+    "url": "/static/media/poker.a2c4a72b.jpg"
+  },
+  {
+    "revision": "514ecb1f97d0558bcbfe53931654576f",
+    "url": "/static/media/profilepic.514ecb1f.jpg"
+  },
+  {
+    "revision": "0f09c810bf76d7bb8589a70994d55176",
+    "url": "/static/media/registration.0f09c810.png"
+  },
+  {
+    "revision": "e629ca5796f583832fa4672f89322a1a",
+    "url": "/static/media/speats.e629ca57.png"
+  },
+  {
+    "revision": "2817387af989f52c8e4a7018fa4b70e3",
+    "url": "/static/media/waldo.2817387a.jpg"
+  }
+]);
